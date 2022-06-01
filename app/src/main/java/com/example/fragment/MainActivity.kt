30cf2b00f4c5be2fb24277c7a57c6429f0c2baa9
MAKSIM <jhonnyamplituda@gmail.com>
2022-06-01 15:43:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import com.example.fragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding // объявляем
+    private val dataModel: DataModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater) // инициализируем
+        setContentView(binding.root)
+        openFrag(BlankFragment.newInstance(), R.id.place_holder)
+        openFrag(BlankFragment2.newInstance(), R.id.place_holder2)
+        dataModel.messageForActivity.observe(this, {
+            binding.textView.text = it
+        })
+
+    }
+
+    private fun openFrag(f: Fragment, idHolder: Int){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(idHolder, f).commit()
+    }
+
+}
